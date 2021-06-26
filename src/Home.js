6fdf47dff27853af74585b4d3e9a849833764bf2
yash7091 <yashgupta.yg709@gmail.com>
2021-06-26 15:23:29 +0530

    some changes in home

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import {useHistory} from 'react-router-dom';
 import  {db, auth} from './firebase'
 import {useAuth} from './contexts/AuthContext';
 import './Home.css'
+
 function Home() {
     const history =useHistory();
     const {currentUser}=useAuth();
@@ -12,6 +13,8 @@ function Home() {
         db.collection('text').add({
             input:"",
             file_name:"Untitled",
+            created_on:new Date().toString(),
+            updated_on:new Date().toString(),
         }).then((ref)=>{
                 history.push('/'+ref.id)
         }).catch(error=>{
@@ -24,6 +27,7 @@ function Home() {
                 (querySnapshot) => {
                     let temp=[]
                     querySnapshot.forEach((doc) => {
+                        console.log(doc.id)
                         temp.push([doc.id,doc.data()]);
                     });
                     setdocs(temp);
@@ -34,15 +38,31 @@ function Home() {
 
     const clickbtn=(e)=>{
         e.preventDefault();
+        console.log(e.target.value);
         history.push('/'+e.target.value);  
+        
     }
 
     return (
         <div className="home">
             <button onClick={newfile}>New file</button>
+            <div className="row g-lg-2" >
             {docs.map((doc)=>{
-                    return <button value={doc[0]} onClick={clickbtn}>{doc[1].file_name}</button>
+                    return (
+                        <div className="column col-md-6 col-lg-4  p-2  ">
+                            <div className="h-100 w-100 shadow-lg card_row">
+                            <button className="h-100 w-100 btn " value={doc[0]} onClick={clickbtn}>
+                                <h5>    
+                                    {doc[1].file_name}
+                                </h5>
+                                {/* <p>{doc[1].created_on}</p>
+                                <p>{doc[1].updated_on}</p> */}
+                            </button>
+                            </div>
+                        </div>
+                    )
             })}
+            </div>
         </div>
     )
 }
